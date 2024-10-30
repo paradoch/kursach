3a7e74a62bec24140e76039b2b402b49dba13b51
kirusha <kirya.buryy.05@mail.ru>
2024-10-30 11:45:13 +0300

    Initial commit

---
+++ general/trasl.js
@@ -0,0 +1,18 @@
+document.getElementById("readmore").addEventListener("click", function() {
+    window.location.href = "/aboutus/about.html";
+  });
+  
+  document.getElementById("royalhotels").addEventListener("click", function() {
+    window.location.href = "/main/index.html";
+  });
+    document.getElementById("findroom").addEventListener("click", function() {
+      window.location.href = "/findroom/findroom.html";
+    });
+  
+    document.getElementById("viewmore").addEventListener("click", function() {
+    window.location.href = "/findroom/findroom.html";
+    });
+  
+    document.getElementById("viewall").addEventListener("click", function() {
+      window.location.href = "/reviews/reviews.html";
+    });
